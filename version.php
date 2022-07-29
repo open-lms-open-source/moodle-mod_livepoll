8f46e27519867d73450300899417d4f93ed55ef7
Rafael Monterroza <rafael.monterroza@openlms.net>
2022-07-29 18:07:24 +0000

    INT-18181: bump versions for 3.11 mp4

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_livepoll';
-$plugin->release = '3.11.3';
-$plugin->version = 2022041300;
+$plugin->release = '3.11.6';
+$plugin->version = 2022072900;
 $plugin->requires = 2021051700;
 $plugin->maturity = MATURITY_STABLE;
