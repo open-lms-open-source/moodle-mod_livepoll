78c94441d896c71c600e22e7ce61de9dcfea6167
Julian Tovar <julings12@gmail.com>
2023-01-24 10:58:39 -0500

    INT-18643: 4.0 MP1 Version bump

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_livepoll';
 $plugin->release = '4.0.5';
-$plugin->version = 2022122100;
+$plugin->version = 2023012400;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
