56a176f0b919073467cea0eeb99f4d43251e63f3
juanfelipe.orozcoescobar <juanfelipe.orozcoescobar@openlms.net>
2024-06-17 16:37:58 -0500

    INT-19886: 4.3 Version Bump of Open LMS plugins

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_livepoll';
-$plugin->release = '4.1.3';
-$plugin->version = 2024040200;
-$plugin->requires = 2022112800;
+$plugin->release = '4.3.4';
+$plugin->version = 2024061700;
+$plugin->requires = 2023100900;
 $plugin->maturity = MATURITY_STABLE;
